43603802023b8140485ebbfeba871576ed81f768
Nipun Kumar <nipunrajput212@hmail.com>
2025-08-19 11:32:24 +0530

    fixed

--- server.js
+++ server.js
@@ -2,73 +2,35 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const authRoutes = require("./routes/authRoutes");
 const app = express();
 app.use(express.json());
 
 
+
+const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-// Auth routes
-app.use("/api/auth", authRoutes);
-
-// Admin routes
-app.use("/api/admin", adminRoutes);
-
-
-// middleware
-app.use(express.json());
-
-// admin routes
-app.use("/api/admin", adminRoutes);
-// Routes
-
-app.use("/api/admin", adminRoutes);
-// Task routes
 const taskRoutes = require("./routes/tasks");
-app.use("/api/tasks", taskRoutes);
+const projectRoutes = require("./routes/projectRoute");
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(5000, () => console.log("✅ Server running on port 5000"));
-  })
-  .catch(err => console.log("❌ DB Error:", err));
-
-require("dotenv").config();
-
-
-
-
-
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-// Auth routes
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log("MongoDB connected"))
+//   .catch(err => console.log(err));
+//  routes
 app.use("/api/auth", authRoutes);
-
-// Admin routes
 app.use("/api/admin", adminRoutes);
-
-
-// middleware
-app.use(express.json());
-
-// admin routes
 app.use("/api/admin", adminRoutes);
-// Routes
-
-app.use("/api/admin", adminRoutes);
-// Task routes
 app.use("/api/tasks", taskRoutes);
+app.use("/api/projects", projectRoutes);
+
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(5000, () => console.log("✅ Server running on port 5000"));
   })
   .catch(err => console.log("❌ DB Error:", err));
+
+  
+
+
+
+
